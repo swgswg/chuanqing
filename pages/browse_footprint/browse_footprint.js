cb7ba6748b4d98fb7d5746901fb508581a17908f
song <1139989513@qq.com>
2018-05-26 09:06:14 +0800

    chuanqing我的还差3网页

---
+++ pages/browse_footprint/browse_footprint.js
@@ -0,0 +1,93 @@
+// pages/browse_footprint/browse_footprint.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      browseFootprint:[
+        { time: '5月4日', 
+          info: [
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" }
+            ] 
+        },
+        {
+            time: '5月5日', info: [
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" }
+            ]
+        },
+        {
+            time: '5月6日', info: [
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" }
+            ]
+        },
+        {
+            time: '5月7日', info: [
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+                { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" }
+            ]
+        }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/goods_detail/goods_detail.js
@@ -0,0 +1,84 @@
+// pages/goods_detail/goods_detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+        '../../images/swiper.png',
+        '../../images/swiper.png',
+        '../../images/swiper.png'
+    ],
+    commentImg: [
+        '../../images/comment-img.png',
+        '../../images/comment-img.png',
+        '../../images/comment-img.png',
+        '../../images/comment-img.png'
+    ],
+    youLike:[
+        { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+        { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+        { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+        { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+        { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" },
+        { img: '../../images/youlike.png', name: '拉菲尚品波尔多', price: "¥108.00" }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
